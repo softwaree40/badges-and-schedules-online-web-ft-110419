a1f52871905e46d030e18657099887f6e746ca24
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-05 15:45:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,9 +4,10 @@ def badge_maker (name)
   "Hello, my name is #{name}."
 end
 def batch_badge_creator (attendees)
+   new_way = []
     attendees.each do |attend|
     #binding.pry
-    new_way = "Hello, my name is #{attend}."
+    new_way.push("Hello, my name is #{attend}."
     return new_way
    end
    
