ef2628d716ee38158a63fbc93d8c6b163e0493f2
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-05 15:20:35 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+require 'pry'
+def badge_maker (name)
+  "Hello, my name is #{name}."
+end
+def batch_badge_creator (attendees)
+  attendees.each do |attend|
+    #binding.pry
+   return "Hello, my name is #{attend}."
+   end
+end
