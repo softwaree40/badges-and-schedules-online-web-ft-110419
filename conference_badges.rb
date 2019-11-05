d7cf34cfbab8158115c36e37660659eab9bbfb8e
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-05 15:53:53 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,8 +4,30 @@ def badge_maker (name)
   "Hello, my name is #{name}."
 end
 def batch_badge_creator (attendees)
-  attendees.each do |attend|
-    #binding.pry
-   return "Hello, my name is #{attend}."
+   new_way = []
+    attendees.each do |attend|
+    
+    new_way.push("Hello, my name is #{attend}.")
+    
    end
+   return new_way
+end
+def assign_rooms(attendees)
+  nuarray = []
+  counter = 1
+  attendees.each do |name|
+    nuarray.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+    counter += 1
+  end
+  return nuarray
+end
+def printer(attendees)
+   batch_badge_creator(attendees).each do |id|
+    puts id
+  end 
+  
+  assign_rooms(attendees).each do |id|
+    puts id 
+  
+  end
 end
