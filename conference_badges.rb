3b6cf8cb69786350f856fe07f0cd900d7ce37af5
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-05 15:53:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,11 +22,11 @@ def assign_rooms(attendees)
   return nuarray
 end
 def printer(attendees)
-   batch_badge_creator(array).each do |id|
+   batch_badge_creator(attendees).each do |id|
     puts id
   end 
   
-  assign_rooms(array).each do |id|
+  assign_rooms(attendees).each do |id|
     puts id 
   
 end
